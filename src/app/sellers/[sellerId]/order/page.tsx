bf4e2fccd22c6678370122ae4b3b95f88f2685f7
Peter Mosher <mosherpe@gmail.com>
2026-01-31 19:23:48 +0000

    Try fixing this error: `Runtime ReferenceError: Separator is not defined

--- src/app/sellers/[sellerId]/order/page.tsx
+++ src/app/sellers/[sellerId]/order/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useState, use, useEffect, useMemo } from 'react';
@@ -23,6 +22,7 @@ import { useCart } from '@/lib/cart-context';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Separator } from '@/components/ui/separator';
 import Link from 'next/link';
 
 export default function BuyerMenuPage({ params }: { params: { sellerId: string } }) {
