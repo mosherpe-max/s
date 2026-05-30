6fb42660d6a5b82132dcc86075f19df77055e0f7
Peter Mosher <mosherpe@gmail.com>
2026-05-30 17:09:48 +0000

    Try fixing this error: `Build Error: You cannot have two parallel pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,8 @@
-
-'use client';
-
-import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
-
 /**
  * DEPRECATED: This route has been moved to /src/app/login/page.tsx
  * to resolve route group resolution issues and parallel page conflicts.
+ * 
+ * To resolve the "parallel pages" build error, this file must not 
+ * export a default component.
  */
-export default function DeprecatedAuthLoginPage() {
-  useEffect(() => {
-    redirect('/login');
-  }, []);
-  
-  return null;
-}
+export {};
